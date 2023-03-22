3d6d13534925195bccc51531a4826be2b38e2b79
Areg2001 <107401750+Areg2001@users.noreply.github.com>
2023-03-22 20:05:11 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from db import Database
 from user import User
+import hashlib
 
-user = User("asdxxsfdasfd", "213dfc-ddd")
 db = Database()
 db.create_table_users()
+login = input("Create your login: ")
+password = input("Create your password: ")
+user = User(login, password)
 db.insert_values(user)
-db1 = Database()
-db1.insert_values(user)
-#db.cursor.execute("""DROP TABLE users""")
-for values in db.cursor.execute("""SELECT * FROM users"""):
-    print(values)
+
+for i in db.cursor.execute("SELECT * FROM users"):
+    print(i)
+
+print("Would you like to check your login and password?")
+answer = input("yes/no: ")
+
+if answer == "yes":
+    login = input("Please, enter your login. ")
+    password = input("Please, enter your password. ").encode()
+
+    for i in db.cursor.execute(f"""SELECT password FROM users
+            WHERE login == '{login}'"""):
+
+        if hashlib.sha3_256(password).hexdigest() == i[0]:
+            print("It is okay.")
+        else:
+            print("It is not okay.")
--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 from validator import Validator
+import hashlib
 
 
 class User:
@@ -7,8 +8,12 @@ class User:
         Validator.validate_logIn(login)
         Validator.validate_password(password)
         self.login = login
-        self.password = password
+        self.password = User.hash_password(password)
 
+    @staticmethod
+    def hash_password(password):
+        pwd = f"{password}".encode()
+        return hashlib.sha3_256(pwd).hexdigest()
 # for values in cursor.execute("""SELECT * FROM users"""):
 #     print(values)
 # user = User("asdxzcxzc", "213dfc.ddd")
